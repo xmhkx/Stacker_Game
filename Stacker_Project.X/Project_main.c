e5b84781430f85bd18031f5026f87b743c7c3b4d
Matthew Klein <matthewklein333@gmail.com>
2026-04-11 20:27:47 -0500

    working pic too arduino UART coms

--- Stacker_Project.X/Project_main.c
+++ Stacker_Project.X/Project_main.c
@@ -1,50 +1,67 @@
-/*
- * File:   Lab_4B.c
- * Author: matthew klein, Gilbert
- * 
- * 
- * 
- * 
- *
- * Created on April 10, 2026
- */
 
-
-// PIC16F1829 Configuration Bit Settings
-
-// 'C' source line config statements
-
-// CONFIG1
-#pragma config FOSC = INTOSC    // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
-#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
-#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
-#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
-#pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
-#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
-#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
-#pragma config IESO = OFF       // Internal/External Switchover (Internal/External Switchover mode is disabled)
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
-
-// CONFIG2
-#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
-#pragma config PLLEN = OFF      // PLL Enable (4x PLL disabled)
-#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
-#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
-#pragma config DEBUG = OFF      // In-Circuit Debugger Mode (In-Circuit Debugger disabled, ICSPCLK and ICSPDAT are general purpose I/O pins)
-#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
-
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
 
 #include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#define _XTAL_FREQ 4000000UL
 
-void delay_variable_ms(int speed);
+#pragma config FOSC = INTOSC
+#pragma config WDTE = OFF
+#pragma config PWRTE = OFF
+#pragma config MCLRE = ON
+#pragma config CP = OFF
+#pragma config CPD = OFF
+#pragma config BOREN = OFF
+#pragma config CLKOUTEN = OFF
+#pragma config IESO = OFF
+#pragma config FCMEN = OFF
+#pragma config WRT = OFF
+#pragma config PLLEN = OFF
+#pragma config STVREN = ON
+#pragma config BORV = LO
+#pragma config LVP = ON
+
+#define SPBRG_VALUE 25   // 9600 baud at 4 MHz with BRGH = 1
+
+void UART_Init(void)
+{
+    OSCCON = 0x6A;   // 4 MHz internal oscillator
+
+    ANSELA = 0x00;
+    ANSELB = 0x00;
+    ANSELC = 0x00;
+
+    TRISCbits.TRISC4 = 1;   // RX input
+    TRISCbits.TRISC5 = 0;   // TX output
+
+    APFCON0bits.RXDTSEL = 1;   // RX on RC4
+    APFCON0bits.TXCKSEL = 1;   // TX on RC5
+
+    SPBRG = SPBRG_VALUE;
+
+    TXSTAbits.SYNC = 0;   // async mode
+    TXSTAbits.BRGH = 1;   // high speed
+    RCSTAbits.SPEN = 1;   // serial port enable
+    TXSTAbits.TXEN = 1;   // enable transmitter
+    RCSTAbits.CREN = 1;   // enable receiver
+}
+
+void UART_Write(char data)
+{
+    while (!PIR1bits.TXIF) {
+        ;
+    }
+    TXREG = data;
+}
 
 void main(void)
 {
-   
+    UART_Init();
+
+    while (1)
+    {
+        UART_Write('A');
+        __delay_ms(500);
+    }
 }
-
-
